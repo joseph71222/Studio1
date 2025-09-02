42e3ef2178665b4c37c85cb6821c84ac53cd6b4e
joseph71222 <joseph71222@gmail.com>
2025-09-02 11:43:35 +0200

    listo
    listo

---
+++ MinimalAPI/Program.cs
@@ -0,0 +1,37 @@
+using System.Data;
+using MySqlConnector;
+using Scalar.AspNetCore;
+using TodoPSR;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var builder = WebApplication.CreateBuilder(args);
+
+//  Obtener la cadena de conexión desde appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("MySQL");
+
+//  Registrando IDbConnection para que se inyecte como dependencia
+//  Cada vez que se inyecte, se creará una nueva instancia con la cadena de conexión
+builder.Services.AddScoped<IDbConnection>(sp => new MySqlConnection(connectionString));
+
+//Cada vez que necesite la interfaz, se va a instanciar automaticamente AdoDapper y se va a pasar al metodo de la API
+builder.Services.AddScoped<IADO, AdoDapper>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "/openapi/{documentName}.json";
+    });
+    app.MapScalarApiReference();
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ studio.mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace studio.mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
